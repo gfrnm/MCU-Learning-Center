7d3fe48a08d3c1e6d032f0f449ab35b1f07138a1
gfrnm <112483789+gfrnm@users.noreply.github.com>
2026-06-10 15:32:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,31 @@
+function pilihProgram(program){
 
-const materi = [
-{
-tanggal:'09 September 2026',
-judul:'Keperawatan Jiwa',
-ppt:'https://drive.google.com/',
-video:'https://youtube.com/'
-},
-{
-tanggal:'10 September 2026',
-judul:'Maternitas',
-ppt:'https://drive.google.com/',
-video:'https://youtube.com/'
-},
-{
-tanggal:'11 September 2026',
-judul:'Keperawatan Anak',
-ppt:'https://drive.google.com/',
-video:'https://youtube.com/'
-},
-{
-tanggal:'12 September 2026',
-judul:'KMB',
-ppt:'https://drive.google.com/',
-video:'https://youtube.com/'
-}
-];
+document
+.getElementById("portal")
+.classList.remove("hidden");
 
-const grid=document.getElementById('materiGrid');
-document.getElementById('materiCount').innerText=materi.length;
+document
+.getElementById("programTitle")
+.innerText =
+"Program " + program;
 
-function render(data){
-grid.innerHTML='';
-data.forEach(item=>{
-grid.innerHTML += `
-<div class="card">
-<div class="badge">${item.tanggal}</div>
-<h3>${item.judul}</h3>
-<div class="btns">
-<a class="btn drive" target="_blank" href="${item.ppt}">PPT</a>
-<a class="btn youtube" target="_blank" href="${item.video}">Rekaman</a>
+document
+.getElementById("batchList")
+.innerHTML =
+`
+<div class="program-card">
+
+<h3>Maret 2026</h3>
+
+<p>12 Pertemuan</p>
+
+<button>
+
+Pilih Batch
+
+</button>
+
 </div>
-</div>`;
-});
-}
-render(materi);
+`;
 
-document.getElementById('search').addEventListener('input',e=>{
-const q=e.target.value.toLowerCase();
-render(materi.filter(x=>x.judul.toLowerCase().includes(q)));
-});
+}
